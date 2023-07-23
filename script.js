e6b4b98d8dddef254e54d73e699b16f8f170bc47
LakshitaTak <134226199+LakshitaTak@users.noreply.github.com>
2023-07-23 12:01:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function fibonacci(num) { // =2
 	if(num < 2){
 		return num
 	}
+	else{
 	let res = fibonacci(num-1) + fibonacci(num-2) // fib(3) + fib(2) = 1 + 1 =2
 	return res
+	}
 }
 
 module.exports = fibonacci;
